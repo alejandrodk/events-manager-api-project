5aaa556525c77762fedd19cf8a31dbe0c771c46d
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-07-19 23:20:56 -0300

    initial commit

---
+++ app/src/test/java/com/events/api/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
