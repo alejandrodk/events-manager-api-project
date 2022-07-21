2cab1de6787aa1799cc2c7a5862e6f163d5f7550
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-07-20 23:50:31 -0300

    enhance: validate room availability

--- app/src/main/java/com/events/api/events/EventsService.java
+++ app/src/main/java/com/events/api/events/EventsService.java
@@ -1,6 +1,7 @@
 package com.events.api.events;
 
-import com.events.api.rooms.RoomsRepository;
+import com.events.api.rooms.RoomsEntity;
+import com.events.api.rooms.RoomsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,17 +9,20 @@ import java.util.List;
 @Service
 public class EventsService {
     private final EventsRepository repository;
-    private final RoomsRepository roomsRepository;
+    private final RoomsService roomsService;
 
-    public EventsService(EventsRepository repository, RoomsRepository roomsRepository) {
+    public EventsService(EventsRepository repository, RoomsService roomsService) {
         this.repository = repository;
-        this.roomsRepository = roomsRepository;
+        this.roomsService = roomsService;
     }
 
     public EventsEntity create(EventsEntity event) {
-        boolean existsRoom = roomsRepository.existsById(event.getRoom());
+        RoomsEntity room = this.roomsService.get(event.getRoom())
+                .orElseThrow(() -> new RuntimeException("Room not found"));
 
-        if (!existsRoom) throw new RuntimeException("Room not found");
+        boolean available = RoomsService.validateAvailability(event, room);
+
+        if (!available) throw new RuntimeException("Room is not available");
 
         return this.repository.save(event);
     }
--- app/src/main/java/com/events/api/rooms/RoomsService.java
+++ app/src/main/java/com/events/api/rooms/RoomsService.java
@@ -1,8 +1,13 @@
 package com.events.api.rooms;
 
+import com.events.api.events.EventsEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomsService {
@@ -25,5 +30,19 @@ public class RoomsService {
 
     public List<RoomsEntity> list() {
         return this.repository.findAll();
+    }
+
+    public Optional<RoomsEntity> get(int id) {
+        return this.repository.findById(id);
+    }
+
+    public static boolean validateAvailability(EventsEntity event, RoomsEntity room) {
+        LocalTime from = event.getFrom().toLocalTime();
+        LocalTime to = event.getTo().toLocalTime();
+
+        LocalTime open = room.getOpen().toLocalTime();
+        LocalTime close = room.getClose().toLocalTime();
+
+        return from.isAfter(open) && to.isBefore(close);
     }
 }
