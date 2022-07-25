27f11077e1e2e766f822466ed18306931449199e
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-07-24 22:01:44 -0300

    new: implement mongodb

--- app/src/main/java/com/events/api/EventsApiApplication.java
+++ app/src/main/java/com/events/api/EventsApiApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+// TODO: remove enable decorators
 @EnableCaching
 @ImportAutoConfiguration(classes = {
 		CacheAutoConfiguration.class,
-		RedisAutoConfiguration.class
+		RedisAutoConfiguration.class,
+		MongoRepositoriesAutoConfiguration.class
 })
 @EnableScheduling
 public class EventsApiApplication {
