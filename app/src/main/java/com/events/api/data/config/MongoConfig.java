d81516221ba6557c77929d18cc1edb940fc74b05
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-07-25 23:16:42 -0300

    update: update config variables

--- app/src/main/java/com/events/api/data/config/MongoConfig.java
+++ app/src/main/java/com/events/api/data/config/MongoConfig.java
@@ -4,20 +4,26 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value( "${spring.data.mongodb.uri}" )
+    private String mongodbUri;
+    @Value( "${spring.data.mongodb.database}" )
+    private String mongodbDatabase;
+
     @Override
     protected String getDatabaseName() {
-        return "events_api";
+        return mongodbDatabase;
     }
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://root:root@localhost:27018/?authSource=admin");
+        ConnectionString connectionString = new ConnectionString(mongodbUri);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString).build();
         return MongoClients.create(mongoClientSettings);
